0a257accf3ecc280a0d67d108319c2490a648044
Rikukuku <ricky_gland821_0714@yahoo.co.jp>
2019-09-08 12:10:45 +0900

    Implement compress js files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './style.css'
 import './style.scss'
 
+console.log("print msg")
 ReactDOM.render(
   <div>Hello, React!</div>,
   document.getElementById('root')
